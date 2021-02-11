6833eb0e7c11ba56d1e44c956ab2d4b4eb090210
la-strole <la-strole.sovaleto48@gmail.com>
2021-02-11 18:15:45 +0300

    booble

---
+++ booble_search/booble.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int tmp;
+    int unsorted_list[10] = {1,7,5,4,2,3,7,9,2,8};
+    int permutations;
+    do
+    {
+        permutations = 0;
+        for (int i=0; i < 9; i++)
+        {
+            if(unsorted_list[i] > unsorted_list[i+1])
+            {
+                tmp = unsorted_list[i];
+                unsorted_list[i] = unsorted_list[i+1];
+                unsorted_list[i+1] = tmp;
+                permutations += 1;
+            }
+        }
+    }
+    while (permutations != 0);
+   
+    for (int i=0; i< 10; i++)
+    {
+        printf("%i ", unsorted_list[i]);
+    }
+
+}
